f8ed6c5556f629b06aaee3bd66aec9e57a1c51a1
Baptiste <lantranbaptiste@gmail.com>
2024-07-18 14:00:49 +0200

    Ajout du bouton de favoris cote front

--- assets/js/recipe/favorite_recipe.js
+++ assets/js/recipe/favorite_recipe.js
@@ -1,22 +1,51 @@
-function setOrUnsetFavorite(recipeId, csrfToken)
+async function setOrUnsetFavorite(recipeId, csrfToken)
 {
+    //We add this function here to make the user experience smoother. So we can avoid having to wait for the server to answer
+    setIsFavorite(recipeId);
+
     let url = '/recipe/favorite/'+recipeId;
 
     let formData = new FormData();
     formData.append('csrf', csrfToken);
 
-    fetch(url, {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-        },
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-//TODO catch error
-    alert(recipeId);
-    alert(csrfToken);
+    try{
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+            },
+            body: formData
+        })
+        if (response.ok) {
+            const data = await response.json();
+            setIsFavorite(recipeId, data.isFavorite)
+        } else {
+            throw new Error(response.status);
+        }
+    }
+    catch(error)
+    {
+        //TODO: alert error modal
+        alert('An error occured, please try again later.');
+        //We comeback to the initial state because there was an error on the backend
+        setIsFavorite(recipeId);
+
+    }
+}
+
+function setIsFavorite(recipeId, isFavorite = null)
+{
+    const btnLikeIcon = document.getElementById('recipe-like-icon-'+recipeId);
+    const btnNotLikedIcon = document.getElementById('recipe-not-liked-icon-'+recipeId);
+
+    const shouldFavorite = (isFavorite === null) ? btnLikeIcon.classList.contains('hide') : isFavorite;
+
+    // Toggle visibility based on the determined favorite state
+    if (shouldFavorite) {
+        btnLikeIcon.classList.remove('hide');
+        btnNotLikedIcon.classList.add('hide');
+    } else {
+        btnNotLikedIcon.classList.remove('hide');
+        btnLikeIcon.classList.add('hide');
+    }
 }
